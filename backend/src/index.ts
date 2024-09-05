352d0ee4fd07b2be8249ca0f041d3691b476a00d
kamehamehaa0000 <gayush794@gmail.com>
2024-09-06 00:58:38 +0530

    asd

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import './config/env'
@@ -7,37 +7,54 @@ import { mainRouter } from './routes/main.router'
 
 const app = express()
 
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN
-      ? process.env.CORS_ORIGIN.split(' ')
-      : 'http://localhost:5173', 
-    credentials: true,
-  })
-);
+const allowedOrigins = process.env.CORS_ORIGIN
+  ? process.env.CORS_ORIGIN.split(',')
+  : ['http://localhost:5173']
 
+const corsOptions = {
+  origin: function (origin: any, callback: any) {
+    if (!origin) return callback(null, true)
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  credentials: true,
+}
 
+app.use(cors(corsOptions))
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
 app.use(express.static('../public'))
+
 ConnectToDatabase()
 
-app.get('/set-cookie', (req, res) => {
-  res.cookie('testCookie', 'testValue', {
-    httpOnly: true,
-    secure: false,
-    sameSite: 'none',
-  })
-  res.send('Cookie set')
+app.use('/api/v1/', mainRouter)
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
 })
 
-app.get('/read-cookie', (req, res) => {
-  const cookie = req.cookies.testCookie
-  res.send(`Cookie value: ${cookie}`)
-})
-app.use('/api/v1/', mainRouter)
+const url = `https://foodtimere.onrender.com`
+const interval = 30000
+
+function reloadWebsite() {
+  fetch(url)
+    .then((response) => {
+      if (response.ok) {
+        console.log('Pinged backend successfully!!')
+      } else {
+        console.log('Failed to ping backend.')
+      }
+    })
+    .catch((error) => {
+      console.log("Can't ping backend!", error)
+    })
+}
+
+setInterval(reloadWebsite, interval)
 
 app.listen(process.env.PORT, () => {
   console.log('Server started on port - ' + process.env.PORT)
