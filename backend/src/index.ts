61418403ecf153d3849d7677cf92edf53a0edb3a
Aayush Gupta <gayush794@gmail.com>
2025-05-09 23:57:41 +0530

    Update cors policy temporarily

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,23 +7,23 @@ import { mainRouter } from './routes/main.router'
 
 const app = express()
 
-const allowedOrigins = process.env.CORS_ORIGIN
-  ? process.env.CORS_ORIGIN.split(',')
-  : ['http://localhost:5173']
+// const allowedOrigins = process.env.CORS_ORIGIN
+//   ? process.env.CORS_ORIGIN.split(',')
+//   : ['http://localhost:5173']
 
-const corsOptions = {
-  origin: function (origin: any, callback: any) {
-    if (!origin) return callback(null, true)
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  },
-  credentials: true,
-}
+// const corsOptions = {
+//   origin: function (origin: any, callback: any) {
+//     if (!origin) return callback(null, true)
+//     if (allowedOrigins.includes(origin)) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   },
+//   credentials: true,
+// }
 
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
