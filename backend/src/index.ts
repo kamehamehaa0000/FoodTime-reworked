57149d4c8c377de1195d001c79af2d24d0e4f13a
Aayush Gupta <gayush794@gmail.com>
2025-11-14 21:42:44 +0530

    Configure CORS to allow all origins

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,7 +23,9 @@ const app = express()
 //   credentials: true,
 // }
 
-app.use(cors())
+app.use(cors({
+  origin:'*'
+}))
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
